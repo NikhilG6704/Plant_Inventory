757e134b01519510fe50ca52a456982c8e51af25
Nikhil Gupta <ng.nikhil674@gmail.com>
2026-06-21 18:27:39 +0530

    commit

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,6 +1,24 @@
 const Database = require("better-sqlite3");
+const fs = require("fs");
 const path = require("path");
+const os = require("os");
 
-const db = new Database(path.join(__dirname, "../inventory.db"));
+const dbFolder = path.join(os.homedir(), "InventoryData");
+
+if (!fs.existsSync(dbFolder)) {
+  fs.mkdirSync(dbFolder, { recursive: true });
+}
+
+const dbPath = path.join(dbFolder, "inventory.db");
+
+const db = new Database(dbPath);
+
+console.log("Database:", dbPath);
 
 module.exports = db;
+
+///database path(MAC): Users/<username>/InventoryData/inventory.db
+
+///database path(Windows): C:\Users\<username>\InventoryData\inventory.db
+
+///database path(Linux): /home/<username>/InventoryData/inventory.db
